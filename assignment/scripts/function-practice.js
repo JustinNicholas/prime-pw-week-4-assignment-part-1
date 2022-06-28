6e4471a70a8bc9e2bd84b6b12e402799e331cb9b
Justin Nicholas <nicholasj964@yahoo.com>
2022-06-27 20:42:15 -0500

    completed Q8 & Q9

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -81,7 +81,7 @@ function find( value, array ){
   }
 }
 
-console.log('This should log false, true, false, false, flase', find( 2, [ 10, 2, 6, 3, 5]));
+console.log('This should log false, true, false, false, false', find( 2, [ 10, 2, 6, 3, 5]));
 
 // ----------------------
 // Stretch Goals
@@ -89,17 +89,28 @@ console.log('This should log false, true, false, false, flase', find( 2, [ 10, 2
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  if ( string.charAt(0) === letter ){
+    return true;
+  } else {
+    return false;
+  }
 
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( 'isFirstLetter where letter is "A" - should say true', isFirstLetter('a', 'apple') );
+console.log( 'isFirstLetter where letter is "Z"- should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
   // TODO: loop to add items
+  for ( i=0; i<array.length; i++ ){
+    sum += array[i];
+  }
+
   return sum;
 }
+
+console.log('This should sum array of [ 1, 2, 3, 4, 5, 6 ] and equal 21. sum =', sumAll([ 1, 2, 3, 4, 5, 6]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
